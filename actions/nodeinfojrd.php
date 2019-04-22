e38ce23cb7b9a2070492362c772de7407eb270c3
Diogo Cordeiro <diogo@fc.up.pt>
2019-04-22 19:15:20 +0100

    Faster NodeInfo stats

--- actions/nodeinfojrd.php
+++ actions/nodeinfojrd.php
@@ -12,8 +12,6 @@ class NodeinfoJRDAction extends XrdAction
 
     protected function setXRD()
     {
-        $this->xrd->links[] = new XML_XRD_Element_link(self::NODEINFO_2_0_REL,  common_local_url('nodeinfo_2_0'));
+        $this->xrd->links[] = new XML_XRD_Element_link(self::NODEINFO_2_0_REL, common_local_url('nodeinfo_2_0'));
     }
 }
-
-
